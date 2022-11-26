30b070542a3738a91045d0559eae5fe96798055c
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2022-11-26 20:30:51 +0700

    merge code Thanh

--- client/src/pages/Home/Footer/index.js
+++ client/src/pages/Home/Footer/index.js
@@ -1,8 +1,29 @@
 import styles from './index.module.css'
 import clsx from 'clsx';
-
-const Footer = () =>{
-
+import { Link } from 'react-router-dom';
+const Footer = () => {
+    return (
+        <Footer className={clsx(styles["footer"])}>
+            <div className={clsx(styles["about"])}>
+                <div className={clsx(styles["about-us"])}>
+                    <h1>Về chúng tôi</h1>
+                    <p>CollectMe</p>
+                </div>
+                <div className={clsx(styles["classify"])}>
+                    <h1>Phân loại</h1>
+                    <Link to='/'>Trang chủ</Link>
+                    <Link to='/'>Tin tức</Link>
+                </div>
+                <div className={clsx(styles["quick-acess"])}>
+                    <h1>Truy cập nhanh</h1>
+                    <Link to='/'>Về chúng tôi</Link>
+                    <Link to='/'>Liên hệ</Link>
+                    <Link to='/'>Đóng góp</Link>
+                    <Link to='/'>Chính sách</Link>
+                </div>
+            </div>
+        </Footer>
+        )
 }
 
 export default Footer;
